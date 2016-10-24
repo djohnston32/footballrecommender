f007a024cf7eafa4c093bc0003551e05eb871a76
djohnston32 <devinjohnston17@gmail.com>
2016-10-24 15:29:44 -0400

    Added dates to schedule

--- recommender/apiTest.py
+++ recommender/apiTest.py
@@ -31,18 +31,24 @@ yardline = gamestate["lineOfScrimmage"] # dict
 - Produce and sort list with every response
     - Test execution time of algorithm so far (less than 1 minute essential)
     - Keep track of last update time and don't run algorithm if unchanged
-    - Pull from api every minute, run algorithm, pretty print results
+    - Pull from local data every minute, run algorithm, pretty print results
     ^ Optimize as necessary (potentially use a database instead of local objects)
+
+*** Thursday 10/26 at 11:55 PM
 
 - Get working as web app with local data
     - Make server get next result from local file every minute and run parseAndSort
     ^ Find way to send output from parseAndSort to frontend to display
     - Make frontend look nice
 
+*** Saturday 10/29 at 11:55 PM
+
 - Get web app working on real time data
     ^ Write startup and exit functions to cleanly begin and end polling
     - Poll from NFL Scoreboard feed every minute and run parseAndSort
     ^ Where possible, optimize algorithm to only update objects when necessary
+
+*** Sunday 10/30 at 11:55 PM
 
 - *Optional* Use Materialize to improve look and feel
 
@@ -62,19 +68,29 @@ yardline = gamestate["lineOfScrimmage"] # dict
     ^ Ask user basic questions on why game is in wrong spot (e.g. score not close enough, too much time, don't like teams)
     - Adjust weighting based on response
 
+*** Saturday 11/05 at 11:55 PM
+
+- Testing and adjusting
+    - Use app throughout NFL Sunday
+    - Address problems as necessary
+
+*** Sunday 11/06 at 11:55 PM
+
 ***Note***
     - Minimum Viable Product acheived at this point
     - Everything after is gravy
     - Sanding rough edges most important, then fine tuning to look more impressive
 
-- General bug fixing
+- General bug fixing - Week of 11/07
     - Fix any remaining persistent bugs
     - Improve aspects that feel shoddy, but may not necessarily be bugs
 
-- *Optional* Fine tuning
+- *Optional* Fine tuning: 11/14 - 11/30
     - Continue to tune default weighting
     ^ Continue to tune feedback mechanism
     - Improve UI
+
+- Prepare deliverables: 12/01 - 12/08
 """
 
 
